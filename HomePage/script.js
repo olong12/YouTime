745f5b86936ccbd5951597e295e56f7d678df3a9
Sergio W. Peterson <Sergiopeter2016@berkeley.edu>
2020-10-19 01:17:39 -0700

    adding a little more

--- HomePage/script.js
+++ HomePage/script.js
@@ -1,12 +1,21 @@
 const WINDOW_LENGTH = window.innerWidth
 const WINDOW_HEIGHT = window.innerHeight
 const NUM_OF_STARS = 50
-const scroll_icon = document.getElementsByClassName('fa-caret-down')[0]
+// let scroll_icon = $('.fa-caret-down')[0]
+// let scroll_icon_container = $('.scroll_icon')[0]
 let large = false
 let starW = 0
 let starH = 0
 
+// scroll_icon_container.addEventListener("mouseover", onEnter, false);
+// scroll_icon_container.addEventListener("mouseout", onLeave, false);
 
+function onEnter(){
+    scroll_icon.style.visibility = 'visible'
+}
+function onLeave(){
+    scroll_icon.style.visibility = 'hidden'
+}
 
 for(i = 0; i < NUM_OF_STARS; i++){
     if (large){
