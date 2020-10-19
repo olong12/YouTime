1f3eb95504b02baa4a4f0bd344f341328a73d8ff
Sergio W. Peterson <Sergiopeter2016@berkeley.edu>
2020-10-19 11:20:52 -0700

    linking new page

--- HomePage/script.js
+++ HomePage/script.js
@@ -1,14 +1,12 @@
 const WINDOW_LENGTH = window.innerWidth
 const WINDOW_HEIGHT = window.innerHeight
 const NUM_OF_STARS = 50
+const start_button = $(".btn")[0]
 // let scroll_icon = $('.fa-caret-down')[0]
 // let scroll_icon_container = $('.scroll_icon')[0]
 let large = false
 let starW = 0
 let starH = 0
-
-// scroll_icon_container.addEventListener("mouseover", onEnter, false);
-// scroll_icon_container.addEventListener("mouseout", onLeave, false);
 
 function onEnter(){
     scroll_icon.style.visibility = 'visible'
@@ -35,3 +33,11 @@ for(i = 0; i < NUM_OF_STARS; i++){
 function makeStar(sw,sh,x,y){
     // 
 }
+
+
+
+// scroll_icon_container.addEventListener("mouseover", onEnter, false);
+// scroll_icon_container.addEventListener("mouseout", onLeave, false);
+start_button.addEventListener("click",function(){
+    window.location.href = '../MainPage/index.html'
+})
