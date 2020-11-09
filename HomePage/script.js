98f2445ade7aadbb155bf0e9e08177cd57c622bd
Sergio W. Peterson <Sergiopeter2016@berkeley.edu>
2020-11-08 17:59:16 -0800

    modified home page

--- HomePage/script.js
+++ HomePage/script.js
@@ -2,14 +2,18 @@ const WINDOW_LENGTH = window.innerWidth
 const WINDOW_HEIGHT = window.innerHeight
 const start_button = $(".btn")[0]
 const backgrounds_url = ['../Img/Background_1.png', '../Img/Background_2.png']
+
 // let scroll_icon = $('.fa-caret-down')[0]
 // let scroll_icon_container = $('.scroll_icon')[0]
 
+
+$(".example_points1")[0].innerHTML = Math.floor(Math.random() * 91)+10
+$(".example_points2")[0].innerHTML = Math.floor(Math.random() * 91)+10
+$(".example_points3")[0].innerHTML = Math.floor(Math.random() * 91)+10
 function setBackground(){
     background = Math.floor(Math.random() * backgrounds_url.length)
     document.body.style.backgroundImage = "url("+backgrounds_url[background]+")"
 }
-
 function onEnter(){
     scroll_icon.style.visibility = 'visible'
 }
